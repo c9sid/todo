1abdc9d3f2e32e3ae5b66bbf0c5e645a3737304e
c9sid <kumarsiddharth7666@gmail.com>
2025-01-23 03:47:28 +0530

    added add todo

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,34 @@
 "use client"
 
+import { Button } from '@/components/ui/button';
 import { Card, CardDescription, CardTitle } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from '@/components/ui/input';
+import { Plus } from 'lucide-react';
+
 import React, { useEffect, useState } from 'react'
+import { toast } from 'sonner';
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
+
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+
   const allTodos = async () => {
     try {
-      const res = await fetch('https://todo-pi-teal.vercel.app/api/todos/', { cache: "no-store" })
+      const res = await fetch('http://localhost:3000/api/todos/', { cache: "no-store" })
       const data = await res.json();
       setTodos(data);
     } catch (error) {
-      console.error(error, "Error while fetching todos")
+      toast.error(error, "Error while fetching todos")
     }
   }
 
@@ -19,15 +36,72 @@ const Home = () => {
     allTodos()
   }, [])
 
+  const addTodo = async () => {
+    if (title == "" || desc == "") {
+      toast.error("Both are required")
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/todos/', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          desc
+        })
+      });
+      if (!res.ok) {
+        throw new Error("Failed to add todo");
+      }
+
+      const newTodo = await res.json();
+      setTitle('')
+      setDesc('')
+      toast.success(`${title} added`)
+      return Response.json(newTodo)
+    } catch (error) {
+      toast.error("Can't create todo", error)
+    }
+  }
+
   return (
     <div className='py-5'>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-        {todos.length > 0 && todos.map((todo) => (
-          <Card key={todo.id} className="p-5">
-            <CardTitle>{todo.title}</CardTitle>
-            <CardDescription>{todo.desc}</CardDescription>
-          </Card>
-        ))}
+      <h2 className='py-5 text-2xl text-center'>Todos ({todos.length})</h2>
+      <div>
+        {todos.length > 0 && todos ? todos.map((todo) => (
+          <div key={todo.id} className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            <Card className="p-5">
+              <CardTitle>{todo.title}</CardTitle>
+              <CardDescription>{todo.desc}</CardDescription>
+            </Card>
+          </div>
+        )) :
+          <div className='grid'>
+            <p className='text-center text-neutral-500'>Not found</p>
+          </div>
+        }
+      </div>
+
+      {/* Add todo */}
+      <div className="fixed bottom-5 right-5">
+        <form>
+          <Dialog>
+            <DialogTrigger className='p-4 rounded-full bg-primary text-white'><Plus /></DialogTrigger>
+            <DialogContent className="w-11/12 rounded-lg">
+              <DialogHeader>
+                <DialogTitle className="pb-5">Add todo</DialogTitle>
+                <DialogDescription className="flex flex-col gap-3">
+                  <Input onChange={(e) => setTitle(e.target.value)} value={title} placeholder="Enter title" />
+                  <Input onChange={(e) => setDesc(e.target.value)} value={desc} placeholder="Enter description" />
+                  <Button onClick={addTodo}>Add Todo</Button>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </form>
       </div>
     </div>
   )
